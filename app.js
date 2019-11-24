c394968c3a1b50a13f0f985a83e192f633a79c71
okrzykawski <57077898+okrzykawski@users.noreply.github.com>
2019-11-25 00:42:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,66 +3,6 @@ const weatherContent = document.querySelector('#weather');
 let city = '';
 const keyAPI = '79fd6ae10a619a738ee3af4fd3149be9';
 
-const getDataWeather = city => {
-    const urlAPI = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${keyAPI}&units=metric`;
-
-    fetch(urlAPI)
-        .then(response =>{
-            if(response.ok) return response;
-            throw Error("Nie udało się")
-        })
-        .then(response => response.json())
-        .then(response => setDataWeather(response))
-        .catch(err => console.log(err))
-        
-    // console.log(urlAPI); 
-};
-const createContentWeather = () =>{
-    const content = document.createElement('div');
-    content.setAttribute('id','content-weather');
-    weatherContent.appendChild(content);
-}
-
-const createHeaderWeatherContent = () =>{
-    const header = document.createElement('h3'); 
-    const headerText = document.createTextNode(`Pogoda dla miasta: ${city}`);
-    header.appendChild(headerText);
-    weatherContent.appendChild(header);
-}
-
-const createP = (elNode, textNode) =>{
-    const p = document.createElement('p');
-    p.appendChild(textNode);
-    elNode.appendChild(p);
-}
-
-const dateWeather = elNode =>{
-    // console.log(new Date().toLocaleString());  
-    const pText = document.createTextNode("Data: " + new Date().toLocaleString());
-    createP(elNode,pText);
-};
-
-const setDataWeather = data =>{
-    const weatherContentChildren = weatherContent.children.length;
-    console.log(weatherContentChildren);
-    if(weatherContentChildren){
-        weatherContent.firstElementChild.remove();
-        createHeaderWeatherContent();
-    }
-    if(weatherContentChildren==2){
-        weatherContent.firstElementChild.nextElementSibling.remove();
-    }
-
-    createContentWeather();
-    const weatherContentDiv = weatherContent.lastElementChild;
-
-    dateWeather(weatherContentDiv);
-
-    const pressure = document.createTextNode(`Ciśnienie: ${data.main.pressure} hPa`);
-    createP(weatherContentDiv,pressure);
-    
-};
-
 weatherForm.addEventListener('submit',function(e){
     e.preventDefault();
     city = this.querySelector('#city').value;
@@ -72,3 +12,80 @@ weatherForm.addEventListener('submit',function(e){
         getDataWeather(city);
     }
 });
+
+const getDataWeather = city => {
+    const urlAPI = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${keyAPI}&units=metric`;
+
+    fetch(urlAPI)
+        .then(response =>{
+            if(response.ok) return response.json();
+            throw Error("Nie udało się")
+        })
+        .then(response => setDataWeather(response))
+        .catch(err => console.log(err))
+        
+    // console.log(urlAPI); 
+};
+
+const setDataWeather = data => {
+    const weatherData = {
+        temp: data.main.temp,
+        pressure: data.main.pressure,
+        wind: data.wind.speed,
+        sunrise: data.sys.sunrise, 
+        sunset: data.sys.sunset 
+    };
+
+    console.log(city, weatherData);
+}
+
+
+
+
+
+
+// const createContentWeather = () =>{
+//     const content = document.createElement('div');
+//     content.setAttribute('id','content-weather');
+//     weatherContent.appendChild(content);
+// }
+
+// const createHeaderWeatherContent = () =>{
+//     const header = document.createElement('h3'); 
+//     const headerText = document.createTextNode(`Pogoda dla miasta: ${city}`);
+//     header.appendChild(headerText);
+//     weatherContent.appendChild(header);
+// }
+
+// const createP = (elNode, textNode) =>{
+//     const p = document.createElement('p');
+//     p.appendChild(textNode);
+//     elNode.appendChild(p);
+// }
+
+// const dateWeather = elNode =>{
+//     // console.log(new Date().toLocaleString());  
+//     const pText = document.createTextNode("Data: " + new Date().toLocaleString());
+//     createP(elNode,pText);
+// };
+
+// const setDataWeather = data =>{
+//     const weatherContentChildren = weatherContent.children.length;
+//     console.log(weatherContentChildren);
+//     if(weatherContentChildren){
+//         weatherContent.firstElementChild.remove();
+//         createHeaderWeatherContent();
+//     }
+//     if(weatherContentChildren==2){
+//         weatherContent.firstElementChild.nextElementSibling.remove();
+//     }
+
+//     createContentWeather();
+//     const weatherContentDiv = weatherContent.lastElementChild;
+
+//     dateWeather(weatherContentDiv);
+
+//     const pressure = document.createTextNode(`Ciśnienie: ${data.main.pressure} hPa`);
+//     createP(weatherContentDiv,pressure);
+    
+// };
