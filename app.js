ea9ebb9827de1ef19b13d99288fa99317cdd4f3f
okrzykawski <57077898+okrzykawski@users.noreply.github.com>
2019-11-29 03:11:05 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const cities = ["Szczecin", "Gdańsk", "Olsztyn", "Białystok", "Poznań", "Bydgoszcz", "Warszawa", "Lublin", "Zielona Góra", "Łódź", "Kielce", "Rzeszów", "Wrocław", "Opole", "Katowice", "Kraków"];
 
 const keyAPI = '79fd6ae10a619a738ee3af4fd3149be9';
-const weatherForm = document.querySelector('#weatherForm form');
+const weatherForm = document.querySelector('#checkWeather form');
 const weatherContent = document.querySelector('#cities > .container > .row');
 
-// let city = '';
+let city = '';
+let submit = false;
 
-// weatherForm.addEventListener('submit', function(e){
-    //     e.preventDefault();
-    //     city = this.querySelector('#city').value;
+weatherForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    city = this.querySelector('#city').value;
     
-    //     if(city){
-        //         getDataWeather(city);
-        //     }
-        // });
+    if(city){
+        const node = document.querySelector("#chooseCity .container .row");
+        submit = true;
+        getDataWeather(city, node);
+    }
+});
         
 
 
@@ -21,10 +24,11 @@ dateWeather(weatherContent);
 
     
 cities.map(city => {
+    submit = false;
     // console.log(city);
     addDivContainer(weatherContent);
-    const node = weatherContent.querySelector(".col-3:last-child");
-    createP(node, city);
+    const node = weatherContent.querySelector(".col-3:last-child > div");
+    createH(node, city);
     getDataWeather(city, node);
     // console.log(city, dataWeather);
 });
--- function.js
+++ function.js
@@ -1,6 +1,5 @@
 const getDataWeather = async (city, node) => {
     const urlAPI = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${keyAPI}&units=metric`;
-    
     const dw = await fetch(urlAPI)
         .then(response =>{
             // console.log("response", response);
@@ -10,7 +9,7 @@ const getDataWeather = async (city, node) => {
         // .then(response => setDataWeather(city, node, response))
         .catch(err => console.log(err));
 
-        console.log(city, dw);
+        // console.log(city, dw, node);
         
         setDataWeather(city, dw, node);
 
@@ -19,7 +18,6 @@ const getDataWeather = async (city, node) => {
 
 const setDataWeather = (city, data, node) => {
     // const weatherDataNode = headerWeatherContent(city, true);
-    
     const weatherData = {
         temp: data.main.temp,
         pressure: data.main.pressure,
@@ -27,18 +25,26 @@ const setDataWeather = (city, data, node) => {
         sunrise: new Date(data.sys.sunrise * 1000).toLocaleTimeString(), 
         sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString() 
     };
+    // console.log("setDataWeather, wetherdata",city, weatherData, node);
     
     // console.log(city, weatherData);
+    if(submit){
+        node.innerHTML="";
+        node.firstChildElement.textContent = city;
+        // node.parentNode.removeChild(node.firstChildElement);
+        // createH(node, city);
+    }
     printDataWeather(weatherData, node);
 };
 
 const printDataWeather = (data, node) =>{
-    const charDegreesC = '&#176;' +"C";
-    createP(node, `Temperatura: ${data.temp} `+ charDegreesC);
-    createP(node, `Ciśnienie: ${data.pressure} hPa`);
-    createP(node, `Siła wiatru: ${data.wind} m/s`);
-    createP(node, `Wschód: ${data.sunrise}`);
-    createP(node, `Zachód: ${data.sunset}`);
+    // const charDegreesC = '&#176;' +"C";
+    // createP(node, `Temperatura: ${data.temp} `+ charDegreesC);
+    createP(node, "Temperatura: ", data.temp, " C");
+    createP(node, "Ciśnienie: ", data.pressure, " hPa");
+    createP(node, "Siła wiatru: ", data.wind, " m/s");
+    createP(node, "Wschód: ", data.sunrise, "");
+    createP(node, "Zachód: ", data.sunset, "");
 }
 
 // const headerWeatherContent = (print, flaga) =>{
@@ -51,23 +57,60 @@ const printDataWeather = (data, node) =>{
 //     // return weatherDataNode;
 // }
 
-const createP = (elNode, textNode) =>{
+const createP = (elNode, text, x, addText) =>{
     const p = document.createElement('p');
-    const pText = document.createTextNode(textNode);
+    const strong = document.createElement('strong');
+    const strongText = document.createTextNode(text);
+    const pText = document.createTextNode(x + addText);
+
+    strong.classList.add("mr-2")
+    if(submit){
+        p.classList.add("col");
+        p.classList.add("my-3");
+        p.classList.add("mx-auto");
+    }
+
+    strong.appendChild(strongText);
+    p.appendChild(strong);
     p.appendChild(pText);
     elNode.appendChild(p);
 }
 
+const createH = (elNode, textNode) =>{
+    const h = document.createElement('h3');
+    const hText = document.createTextNode(textNode);
+    h.classList.add("text-center")
+    h.classList.add("pb-1")
+    h.style.borderBottom = "1px solid #44f";
+    // if(submit) {
+    //     h.classList.add("col");
+    //     h.style.borderBottom = "none";
+    // }
+    // h.style.borderBottomStyle = "solid";
+    // h.style.borderBottomColor = "red";
+    h.appendChild(hText);
+    if(submit){
+        elNode.parentNode.insertBefore(h, elNode);
+    }else{
+        elNode.appendChild(h);
+    }
+}
+
 const dateWeather = () =>{
     const date = new Date().toLocaleString();
-    const dateNode = weatherContent.previousElementSibling;
+    const dateNode =document.querySelector("#dateWeather .container p");
     dateNode.textContent = date ;
 };
 
 const addDivContainer = node =>{
     const el = document.createElement("div");
+    const el2 = document.createElement("div");
     el.classList.add("col-3");
     el.classList.add("my-4");
-    el.classList.add("shadow");
+
+    el.appendChild(el2);
+    el2.classList.add("shadow-lg");
+    el2.classList.add("p-3");
+
     node.appendChild(el);
 };
